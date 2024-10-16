728a7b3b1e79173360afad6b770a9ce502c82c64
JJHazem <110510460+JJHazem@users.noreply.github.com>
2024-10-16 20:10:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Preflight request handling (OPTIONS requests)
-app.options('*', cors(corsOptions)); // This ensures proper handling of CORS preflight
+app.options(cors(corsOptions)); // This ensures proper handling of CORS preflight
 
 // Security and Cache-Control headers for all responses
 app.use((req, res, next) => {
