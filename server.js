f3ee53c1f751be8e2e36b53d0b87cf80d05bb179
JJHazem <110510460+JJHazem@users.noreply.github.com>
2024-10-19 00:39:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const port = 3000;
 // Initialize app and middleware
 const app = express();
 
-app.use(cors({ origin: 'https://capitalhillsdevelopments.com' })); 
+app.use(cors({
+    origin: 'https://capitalhillsdevelopments.com',
+    methods: 'GET,POST,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+}));
 // Connect to MongoDB (replace with your actual MongoDB URI)
 mongoose.connect('mongodb://37.148.206.181:27017/capital', {
     useNewUrlParser: true,
