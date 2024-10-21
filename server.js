cd6a8209cf1220cda64fb7e577c5778bbdfa91aa
JJHazem <110510460+JJHazem@users.noreply.github.com>
2024-10-21 21:11:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,13 +26,6 @@ const corsOptions = {
     credentials: true  // Allow cookies/auth tokens to be sent
 };
 app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://capitalhillsdevelopments.com"); // Adjust to the origin you want to allow
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 app.use(bodyParser.json());
 app.use(express.json({ limit: '10mb' })); 
 
