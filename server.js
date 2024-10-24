c878429497517f92bdaeac191f4baa7dde8e3a5f
JJHazem <110510460+JJHazem@users.noreply.github.com>
2024-10-25 02:08:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors(corsOptions));
 app.use(express.json()); // For parsing application/json
 
 
-mongoose.connect('mongodb://admin:CHDahmed135@37.148.206.181:27017/capital', {
+mongoose.connect('mongodb://admin:CHDahmed135@37.148.206.181:27017/capital?authSource=admin', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
